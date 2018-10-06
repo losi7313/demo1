f654262662f7637377317264e335246f32cb02e7
losi7313 <losig7313@gmail.com>
2018-10-06 13:24:59 +0300

    feature (entity): customize User

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -4,22 +4,24 @@ namespace App\Entity;
 
 use FOS\UserBundle\Model\User as BaseUser;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity
  * @ORM\Table(name="user")
+ * @ORM\HasLifecycleCallbacks()
  * @ORM\AttributeOverrides({
  *      @ORM\AttributeOverride(name="usernameCanonical",
  *          column=@ORM\Column(
  *              name     = "username_canonical",
- *              length   = 191,
+ *              length   = 50,
  *              unique   = true
  *          )
  *      ),
  *      @ORM\AttributeOverride(name="emailCanonical",
  *          column=@ORM\Column(
  *              name     = "email_canonical",
- *              length   = 191,
+ *              length   = 100,
  *              unique   = true
  *          )
  *      )
@@ -28,9 +30,180 @@ use Doctrine\ORM\Mapping as ORM;
 class User extends BaseUser
 {
     /**
+     * @var int
      * @ORM\Id
      * @ORM\Column(type="integer")
      * @ORM\GeneratedValue(strategy="AUTO")
      */
     protected $id;
+
+    /**
+     * @var string
+     * @ORM\Column(type="string", length=100)
+     * @Assert\NotBlank()
+     */
+    private $name;
+
+    /**
+     * @var string
+     * @ORM\Column(type="string", length=100)
+     * @Assert\NotBlank()
+     * @Assert\Length(min = 8, max = 20)
+     * @Assert\Regex(pattern="/^\+*\(*\)*[0-9]*$/", message="number_only")
+     */
+    private $phoneNo;
+
+    /**
+     * @var string
+     * @ORM\Column(type="text")
+     */
+    private $description;
+
+    /**
+     * @var string
+     * @ORM\Column(type="string", length=200)
+     * @Assert\File(
+     *     maxSize = "3M",
+     *     mimeTypes={"image/jpeg", "image/png"}
+     *     )
+     */
+    private $photo;
+
+    /**
+     * @var \DateTime
+     * @ORM\Column(type="datetime")
+     */
+    private $createdAt;
+
+    /**
+     * @var \DateTime
+     * @ORM\Column(type="datetime")
+     */
+    private $updatedAt;
+
+    /**
+     * @return null|string
+     */
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     * @return User
+     */
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * @return null|string
+     */
+    public function getPhoneNo(): ?string
+    {
+        return $this->phoneNo;
+    }
+
+    /**
+     * @param string $phoneNo
+     * @return User
+     */
+    public function setPhoneNo(string $phoneNo): self
+    {
+        $this->phoneNo = $phoneNo;
+
+        return $this;
+    }
+
+    /**
+     * @return null|string
+     */
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    /**
+     * @param string $description
+     * @return User
+     */
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    /**
+     * @return null|string
+     */
+    public function getPhoto(): ?string
+    {
+        return $this->photo;
+    }
+
+    /**
+     * @param string $photo
+     * @return User
+     */
+    public function setPhoto(string $photo): self
+    {
+        $this->photo = $photo;
+
+        return $this;
+    }
+
+    /**
+     * @return \DateTimeInterface|null
+     */
+    public function getCreatedAt(): ?\DateTimeInterface
+    {
+        return $this->createdAt;
+    }
+
+    /**
+     * @param \DateTimeInterface $createdAt
+     * @return User
+     */
+    public function setCreatedAt(\DateTimeInterface $createdAt): self
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    /**
+     * @return \DateTimeInterface|null
+     */
+    public function getUpdatedAt(): ?\DateTimeInterface
+    {
+        return $this->updatedAt;
+    }
+
+    /**
+     * @param \DateTimeInterface $updatedAt
+     * @return User
+     */
+    public function setUpdatedAt(\DateTimeInterface $updatedAt): self
+    {
+        $this->updatedAt = $updatedAt;
+
+        return $this;
+    }
+
+    /**
+     * @ORM\PreFlush()
+     */
+    public function preFlush()
+    {
+        // set timestamps
+        $this->setUpdatedAt(new \DateTime());
+        if ($this->getCreatedAt() === null) {
+            $this->setCreatedAt($this->getUpdatedAt());
+        }
+    }
 }
---
+++ src/Form/RegistrationType.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use FOS\UserBundle\Form\Type\RegistrationFormType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class RegistrationType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('name')
+            ->add('description')
+            ->add('phoneNo')
+            ->add('photo', FileType::class);
+    }
+
+    public function getParent()
+    {
+        return RegistrationFormType::class;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(
+            array(
+                'data_class' => User::class,
+            )
+        );
+    }
+}
